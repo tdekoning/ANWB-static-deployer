92bc05b85e3ecfc09679a7f815d20557912942a0
= <=>
2011-10-07 13:51:53 +0200

    Testserver verbind naar localhost

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ var http = require('http');
 http.createServer( function ( req, res ) {
 
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('Hello World2\n');
 
-}).listen(1337, "192.168.56.101");
+}).listen(1337, "127.0.0.1");
 
-console.log('Server running at http://192.168.56.101:1337/');
+console.log('Server running at http://127.0.0.1:1337/');
