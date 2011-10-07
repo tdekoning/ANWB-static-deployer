f564a975b41b712db585547870e90dce27a7148a
= <=>
2011-10-07 13:32:39 +0200

    Testcommit.

---
+++ test.js
@@ -0,0 +1,6 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1337, "192.168.56.101");
+console.log('Server running at http://192.168.56.101:1337/');
