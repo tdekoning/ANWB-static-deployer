396dc9df5bff4f7b0f24608fe49a1d204b83228d
Ted de Koning <teddk23@gmail.com>
2011-10-07 14:08:06 +0200

    nummer 2 weggehaald

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var http = require('http');
 http.createServer( function ( req, res ) {
 
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World2\n');
+  res.end('Hello World\n');
 
 }).listen(1337, "127.0.0.1");
 
