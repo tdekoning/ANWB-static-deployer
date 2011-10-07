cfc66001e838ce4c37283fd156e15a646c838c5a
Guus Goossens <guus.goossens@gmail.com>
2011-10-07 18:37:45 +0200

    Params: als prefix gebruikt zodat je weet wat je logged

--- server/apiRequest.js
+++ server/apiRequest.js
@@ -4,7 +4,7 @@ exports.performRequest = function( req, res ) {
 	//req moet action als parameter hebben.
 	//Action mapped op api functie.
 	var parsedUrl = url.parse( req.url );
-	console.log( getParameters( parsedUrl ) );
+	console.log( 'params: ', getParameters( parsedUrl ) );
 }
 
 /*
