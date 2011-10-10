46ea485bb2b2a0d5f5973f0a3e7546619b8b3761
Ted de Koning <teddk23@gmail.com>
2011-10-10 11:26:33 +0200

    Whitespace weghalen

--- server/api/deploy.js
+++ server/api/deploy.js
@@ -2,7 +2,7 @@
  * Voer het deployen van de gegeven svnDir naar de gegeven serverUrl uit.
  */
 exports.execute = function ( callback, svnDir, serverUrl ) {
-	
+
 	// TODO: Check code uit (in tmp dir)
 	// TODO: Voer staticjscsscompiler.jar uit.
 	// TODO: Check de verschillen met de gegeven server.
