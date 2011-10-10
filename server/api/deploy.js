d511e4cbf743b40f102d5a07b9bc5389a2d0b257
Ted de Koning <teddk23@gmail.com>
2011-10-10 11:25:11 +0200

    Deployment en dryrun script toevoegen

---
+++ server/api/deploy.js
@@ -0,0 +1,8 @@
+exports.execute = function ( callback, svnDir, serverUrl ) {
+	
+	// TODO: Check code uit (in tmp dir)
+	// TODO: Voer staticjscsscompiler.jar uit.
+	// TODO: Check de verschillen met de gegeven server.
+	// TODO: Voer rsync uit.
+
+}
---
+++ server/api/dryrun.js
@@ -0,0 +1,7 @@
+exports.execute = function ( callback, svnDir, serverUrl ) {
+	
+	// TODO: Check code uit (in tmp dir)
+	// TODO: Voer staticjscsscompiler.jar uit.
+	// TODO: Check de verschillen met de gegeven server.
+
+}
